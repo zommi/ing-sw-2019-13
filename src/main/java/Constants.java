8ea635927c4e522b1859f9520abd3ce5896bad3a
Tommaso Pegolotti <tommaso.pegolotti@mail.polimi.it>
2019-04-07 20:53:20 +0200

    add array of value to store points

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -3,4 +3,6 @@ public class Constants {
     public static final int MAX_MARKS = 3;
     public static final int NUM_FIGURES = 5;
     public static final int MAX_AMMO_CUBES_PER_COLOR = 3;
+    public static final int MAX_NUMBER_OF_DEATHS = 6;
+    public static final int[] POINT_VALUE = {8,6,4,2,1,1};
 }
