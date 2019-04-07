abba152da973147890a56b23dd137239fb85e0c8
Tommaso Pegolotti <tommaso.pegolotti@mail.polimi.it>
2019-04-07 19:19:55 +0200

    add max cube constant

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class Constants {
+    public static final int MAX_HP = 12;
+    public static final int MAX_MARKS = 3;
+    public static final int NUM_FIGURES = 5;
+    public static final int MAX_AMMO_CUBES_PER_COLOR = 3;
 }
