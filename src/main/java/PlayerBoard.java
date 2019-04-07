59a2a94d28357bcbf2cb90b09cc0d83394a74476
Tommaso Pegolotti <tommaso.pegolotti@mail.polimi.it>
2019-04-07 20:54:00 +0200

    add constructor and addSkull

--- src/main/java/PlayerBoard.java
+++ src/main/java/PlayerBoard.java
@@ -4,45 +4,101 @@ import java.util.*;
 /**
  * 
  */
-public class PlayerBoard {
+public class PlayerBoard{
+    ConcretePlayer player;
+
+    private DamageToken[] damage;
+    private int damageTaken;
+
+    private DamageToken[] marks;
+    private int marksOnBoard;
+
+    private int[] pointValue;
+    private int currentPointValueCursor;
+    private int numberOfDeaths;
+
+    private ArrayList<AmmoCube> redAmmo;
+    private int validRedAmmo;
+
+    private ArrayList<AmmoCube> yellowAmmo;
+    private int validYellowAmmo;
+
+    private ArrayList<AmmoCube> blueAmmo;
+    private int validBlueAmmo;
+
 
     /**
      * Default constructor
      */
-    public PlayerBoard() {
+    public PlayerBoard(ConcretePlayer p) {
+        this.player = p;
+        this.damage = new DamageToken[Constants.MAX_HP];
+        this.damageTaken = 0;
+        this.marks = new DamageToken[Constants.MAX_MARKS];
+        this.marksOnBoard = 0;
+        this.numberOfDeaths = 0;
+        this.pointValue = Constants.POINT_VALUE;
+        this.currentPointValueCursor = 0;
+        this.redAmmo = new ArrayList<AmmoCube>();
+        this.validRedAmmo = 0;
+        this.yellowAmmo = new ArrayList<AmmoCube>();
+        this.validYellowAmmo = 0;
+        this.blueAmmo = new ArrayList<AmmoCube>();
+        this.validBlueAmmo = 0;
     }
 
 
     /**
      * @return
      */
-    public int getRemainingHP() {
-        // TODO implement here
-        return 0;
+    public int getDamageTaken() {
+        return damageTaken;
     }
 
     /**
      * @return
      */
     public int getNumberOfDeaths() {
-        // TODO implement here
-        return 0;
+        return numberOfDeaths;
     }
 
     /**
      * @return
      */
-    public List<AmmoCube> getValidAmmoCube() {
-        // TODO implement here
-        return null;
+    public List<AmmoCube> getValidRedAmmoCube() {
+        return (ArrayList<AmmoCube>) redAmmo.clone();
+    }
+
+    public List<AmmoCube> getValidYellowAmmoCube() {
+        return (ArrayList<AmmoCube>) yellowAmmo.clone();
+    }
+
+    public List<AmmoCube> getValidBlueAmmoCube() {
+        return (ArrayList<AmmoCube>) blueAmmo.clone();
     }
 
     /**
      * @return
      */
+    //add easter egg when someone dies 7 times in the same game and make him win.
     public void addSkull() {
-        // TODO implement here
-        return null;
+        if(damageTaken > 10){
+            numberOfDeaths++;
+            currentPointValueCursor++;
+        } else{
+            System.err.println("PLAYER STILL ALIVE");
+        }
     }
 
+    public int getPointValue(){
+        return pointValue[currentPointValueCursor];
+    }
+
+    public void addDamage(List<DamageToken> damage) {
+        //TODO
+        damageTaken += damage.size();
+    }
+
+    public void addMarks(int marks) {
+    }
 }
