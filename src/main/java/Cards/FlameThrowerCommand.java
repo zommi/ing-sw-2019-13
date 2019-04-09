0d5726e3bd7bafdff8184c0ce8527a3d2be37e16
Alessia Paccagnella <alessia.paccagnella@mail.polimi.it>
2019-04-09 23:21:34 +0200

    modified commands

--- src/main/java/Cards/FlameThrowerCommand.java
+++ src/main/java/Cards/FlameThrowerCommand.java
@@ -7,11 +7,56 @@ import Player.Character;
 
 public class FlameThrowerCommand {
 
+    private char direction;
+
     public FlameThrowerCommand() {
     }
 
+    public void setdirection(char c) //i need this method in order to communicate the direction from the controller to the model
+    {
+        this.direction = c;
+    }
+
     public ArrayList<ArrayList<Character>> execute(Square square) {
-        return null;
+
+        ArrayList<Square> eN = (ArrayList<Square>)square.getTwoSquaresInTheSameDirection('n');
+        ArrayList<Square> eS = (ArrayList<Square>)square.getTwoSquaresInTheSameDirection('s');
+        ArrayList<Square> eW = (ArrayList<Square>)square.getTwoSquaresInTheSameDirection('w');
+        ArrayList<Square> eE = (ArrayList<Square>)square.getTwoSquaresInTheSameDirection('e');
+
+        ArrayList<ArrayList<Character>> cN = new ArrayList<ArrayList<Character>>();
+        ArrayList<ArrayList<Character>> cS = new ArrayList<ArrayList<Character>>();
+        ArrayList<ArrayList<Character>> cW = new ArrayList<ArrayList<Character>>();
+        ArrayList<ArrayList<Character>> cE = new ArrayList<ArrayList<Character>>();
+
+
+        for (Square t : eN) {
+            cN.add((ArrayList<Character>)t.getCharacters());
+        }
+
+        for (Square t : eS) {
+            cS.add((ArrayList<Character>)t.getCharacters());
+        }
+
+        for (Square t : eW) {
+            cW.add((ArrayList<Character>)t.getCharacters());
+        }
+
+        for (Square t : eE) {
+            cE.add((ArrayList<Character>)t.getCharacters());
+        }
+
+        if(this.direction == 'n')
+            return cN.clone();
+        else if(this.direction == 's')
+            return cS.clone();
+        else if(this.direction == 'e')
+            return cE.clone();
+        else if(this.direction == 'w')
+            return cW.clone();
+
+        //now the user has to say to me which direction he wants. Otherwise I don't know what to return.
+
     }
 
 }
