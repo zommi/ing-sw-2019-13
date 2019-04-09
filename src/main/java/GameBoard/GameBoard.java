fe465a5e2ea6abea30244149a918d6cc403d9445
Tommaso Pegolotti <tommaso.pegolotti@mail.polimi.it>
2019-04-09 22:19:30 +0200

    add constructor

--- src/main/java/GameBoard/GameBoard.java
+++ src/main/java/GameBoard/GameBoard.java
@@ -1,31 +1,47 @@
 package GameBoard;
 
+import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.*;
-import Map.*;
+
+import Constants.Constants;
+import Map.Map;
 
 /**
  * 
  */
 public class GameBoard {
 
-    private ArrayList<SpawnPoint> spawnPoints;
+    private Map gameMap;
+    private KillshotTrack track;
+    private WeaponDeck weaponDeck;
+    private PowerupDeck powerupDeck;
+    public static GameBoard istance;
 
 
-    public GameBoard() {
-
+    private GameBoard(int mapChoice, int skullChoice) {
+        try {
+            this.gameMap = new Map(mapChoice);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        this.track = new KillshotTrack(skullChoice);
+        this.weaponDeck = new WeaponDeck(Constants.NUMBER_OF_WEAPONS);
+        this.powerupDeck = new PowerupDeck();
     }
 
-
-
-
+    public static GameBoard istance(int mapChoice, int skullChoice){
+        if(istance != null) istance = new GameBoard(mapChoice, skullChoice);
+        return istance;
+    }
 
 
     /**
      * @return
      */
     public void setup() {
-        // TODO implement here
+        weaponDeck.shuffle();
+        powerupDeck.shuffle();
     }
 
 }
