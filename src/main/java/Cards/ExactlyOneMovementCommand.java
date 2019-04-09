507d6877581d5988e11d677e2f22c17fd0001e37
Alessia Paccagnella <alessia.paccagnella@mail.polimi.it>
2019-04-09 14:21:36 +0200

    updated weapons commands

--- src/main/java/Cards/ExactlyOneMovementCommand.java
+++ src/main/java/Cards/ExactlyOneMovementCommand.java
@@ -11,6 +11,10 @@ public class ExactlyOneMovementCommand implements Command {
     public ExactlyOneMovementCommand() {
     }
 
-    public ArrayList<ArrayList<Character>> execute(Square square){return null;}
+    public ArrayList<ArrayList<Character>> execute(Square square){
+        ArrayList<ArrayList<Character>> e = new ArrayList<ArrayList<Character>>();
+        e.add((ArrayList)square.getExactlyOneMovementCharacters();
+        return e;
+    }
 
 }
