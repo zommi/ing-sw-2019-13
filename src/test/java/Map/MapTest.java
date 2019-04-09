8c3df3561e1c6b34880dac1396b5c3c6c4a1b308
Rambo <teopac165@gmail.com>
2019-04-09 23:22:11 +0200

    add map test

--- src/test/java/Map/MapTest.java
+++ src/test/java/Map/MapTest.java
@@ -2,6 +2,9 @@ package Map;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MapTest {
@@ -9,8 +12,11 @@ class MapTest {
     @Test
     public void isMapConstructorOk(){
         Map map = new Map(1);
-
-
+        List<SpawnPoint> spawnPoints = new ArrayList<>();
+        spawnPoints.add(new SpawnPoint(0,3, 'b'));
+        spawnPoints.add(new SpawnPoint(1,0, 'r'));
+        spawnPoints.add(new SpawnPoint(3,4, 'y'));
+        assertEquals(spawnPoints, map.getSpawnPoints());
 
     }
 
