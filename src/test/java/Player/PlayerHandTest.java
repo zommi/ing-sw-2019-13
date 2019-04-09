9d861368f5458e006de78516abdb319e08e79e3e
Tommaso Pegolotti <tommaso.pegolotti@mail.polimi.it>
2019-04-09 14:02:47 +0200

    add test cases

--- src/test/java/Player/PlayerHandTest.java
+++ src/test/java/Player/PlayerHandTest.java
@@ -4,20 +4,22 @@ import Exceptions.InvalidMoveException;
 import GameBoard.GameBoard;
 import org.junit.jupiter.api.Test;
 
+import java.util.Scanner;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PlayerHandTest {
 
     @Test
     public void testPlayCard(){
-        ConcretePlayer p = new ConcretePlayer("pippo",new GameBoard());
+        ConcretePlayer p = new ConcretePlayer("pippo",new GameBoard(), Figure.DESTRUCTOR);
+        assertEquals(Figure.DESTRUCTOR, p.getCharacter().getFigure());
         PlayerHand ph = new PlayerHand(p);
 
           assertThrows(InvalidMoveException.class, () -> {
               ph.playCard(2,'q');
         });
     }
-
     @Test
     public void test(){
         assertEquals(2,2);
