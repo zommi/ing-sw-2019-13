65c97efeb7401c6f629d4d03241791ad30259b90
Alessia Paccagnella <alessia.paccagnella@mail.polimi.it>
2019-04-09 17:02:09 +0200

    modified commands

--- src/main/java/Cards/AimThroughWallsCommand.java
+++ src/main/java/Cards/AimThroughWallsCommand.java
@@ -12,7 +12,6 @@ public class AimThroughWallsCommand implements Command {
     public AimThroughWallsCommand() {
     }
 
-<<<<<<< HEAD
     public ArrayList<ArrayList<Character>> execute(Square square){
         ArrayList<Character> e = (ArrayList)square.getCharactersThroughWalls();
 
@@ -22,10 +21,6 @@ public class AimThroughWallsCommand implements Command {
         ArrayList<Character> eS = new ArrayList<Character>(); //SOUTH
         ArrayList<Character> eW = new ArrayList<Character>(); //WEST
         ArrayList<Character> eE = new ArrayList<Character>(); //EAST
-=======
-
-    public ArrayList<ArrayList<Character>> execute(Square square){return null;}
->>>>>>> 44bd45b03f93d9f0f931d24e52ae979ab25ff80b
 
         //now I have to create an array of array with 4 different lists.
         for (Character t : e) {
