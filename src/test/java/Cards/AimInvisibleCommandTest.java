31bc2548060b47d1728de764d5b0d7a07f0a5c85
alessiapacca <48483036+alessiapacca@users.noreply.github.com>
2019-04-14 15:59:54 +0200

    Deleted the instances of squares

--- src/test/java/Cards/AimInvisibleCommandTest.java
+++ src/test/java/Cards/AimInvisibleCommandTest.java
@@ -12,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AimInvisibleCommandTest {
     @Test
-    void execute() { //execute returns an arraylist of arraylist of non visible char
+    void execute ()  throws Exceptions.NoSuchSquareException{ //execute returns an arraylist of arraylist of non visible char
 
         Character c = new Character(Player.Figure.DESTRUCTOR);
         Character c1 = new Character(Player.Figure.BANSHEE);
@@ -20,23 +20,13 @@ class AimInvisibleCommandTest {
         Character c3 = new Character(Player.Figure.VIOLET);
         Character c4 = new Character(Player.Figure.SPROG);
 
-        /*Map map = new Map(1);
+        Map map = new Map(1);
 
-        Square square0 = new Square(0,0, 'b');
-        square0.addCharacter(new Character(Player.Figure.BANSHEE));
-        Square square1 = new Square(1,0, 'b');
-        square1.addCharacter(new Character(Player.Figure.DOZER));
-        Square square2 = new Square(2,0, 'B');
-        Square square3 = new Square(0,1, 'R' );
-        Square square4 = new Square(1,1, 'r');
-        Square square5 = new Square(2,1, 'r');
-        Square square6 = new Square(3,1, 'y');
-        Square square7 = new Square(1,2, 'w');
-        Square square8 = new Square(2,2, 'w');
-        Square square9 = new Square(3,2, 'Y');
-        square7.addCharacter(new Character(Player.Figure.DESTRUCTOR));
-        square8.addCharacter(new Character(Player.Figure.VIOLET));
-        square9.addCharacter(new Character(Player.Figure.SPROG));
+        Map.getSquareFromXY(0,0).addCharacter(c1);
+        Map.getSquareFromXY(1,0).addCharacter(c2);
+        Map.getSquareFromXY(1,2).addCharacter(c);
+        Map.getSquareFromXY(2,2).addCharacter(c3);
+        Map.getSquareFromXY(3,1).addCharacter(c4);
 
 
         ArrayList<Character> e = new ArrayList<Character>();
@@ -48,6 +38,6 @@ class AimInvisibleCommandTest {
         e1.add(e);
 
         AimInvisibleCommand aim = new AimInvisibleCommand();
-        assertEquals(aim.execute(new Square(0,0,'b')), e);*/
+        assertEquals(aim.execute(new Square(0,0,'b')), e1);
     }
 }
