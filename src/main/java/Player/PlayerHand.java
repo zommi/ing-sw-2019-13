a180cf14c46bc52008bf5f21dbc37666d536e4aa
Alessia Paccagnella <alessia.paccagnella@mail.polimi.it>
2019-04-16 10:41:36 +0200

    uncommented a line that I accidentally commented

--- src/main/java/Player/PlayerHand.java
+++ src/main/java/Player/PlayerHand.java
@@ -46,10 +46,10 @@ public class PlayerHand {
     public void playCard(int choice, char c) throws InvalidMoveException {
         System.out.println("Playing Card...");
         switch (c){
-            //case 'w' : weaponHand.get(choice).play(); //which is a weaponcard
-                //break;
-            //case 'p' : powerupHand.get(choice).play(); //which is a powerupcard
-                //break;
+            case 'w' : weaponHand.get(choice).play(); //which is a weaponcard
+                break;
+            case 'p' : powerupHand.get(choice).play(); //which is a powerupcard
+                break;
             default: throw new InvalidMoveException();
         }
     }
