0584ce3ba5c259fe774c8236c3519738ad797aeb
Tommaso Pegolotti <tommaso.pegolotti@mail.polimi.it>
2019-04-21 18:31:19 +0200

    add first implementations of powerupDeck

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -7,7 +7,9 @@ public class Constants {
     public static final int MAX_AMMO_CUBES_PER_COLOR = 3;
     public static final int MAX_NUMBER_OF_DEATHS = 6;
     public static final int[] POINT_VALUE = {8,6,4,2,1,1};
+
     public static final int NUMBER_OF_WEAPONS = 21;
+    public static final int NUMBER_OF_POWERUP_CARDS = 24;
 
     public static final int NO_POWERUP = -10;
 
--- src/main/java/GameBoard/PowerupDeck.java
+++ src/main/java/GameBoard/PowerupDeck.java
@@ -1,7 +1,7 @@
 package GameBoard;
 
-import Cards.CardInterface;
-import Cards.PowerupCard;
+import Cards.*;
+import Constants.Constants;
 
 import java.util.*;
 
@@ -10,47 +10,43 @@ import java.util.*;
  */
 public class PowerupDeck {
 
-    /**
-     * Default constructor
-     */
-    public PowerupDeck() {
-    }
-
-    /**
-     * 
-     */
+    private LinkedList<PowerupCard> deck;
     public List<PowerupCard> discardedCards;
+    
+    public PowerupDeck() {
+        this.discardedCards = new ArrayList<PowerupCard>();
+        initializeDeck();
+    }
 
 
-    /**
-     * @return
-     */
+    //TODO once done powerup cards finish this
+    public void initializeDeck(){
+        this.deck = new LinkedList<PowerupCard>();
+        for(int i = 0; i < Constants.NUMBER_OF_POWERUP_CARDS; i++){
+        }
+    }
+
+
     public void shuffle() {
-        // TODO implement here
+        Collections.shuffle(this.deck);
     }
 
-    /**
-     * @return
-     */
     public boolean isEmpty() {
-        // TODO implement here
-        return false;
+        return this.deck.isEmpty();
     }
 
-    /**
-     * @return
-     */
     public void restore() {
-        // TODO implement here
+        for(PowerupCard card : discardedCards){
+            this.deck.add(card);
+            this.discardedCards.remove(card);
+        }
     }
 
-    /**
-     * @return
-     */
-    public void discardCard(CardInterface card) {
-        // TODO implement here
+    public void discardCard(PowerupCard card) {
+        this.discardedCards.add(card);
     }
 
-    public void draw() {
+    public PowerupCard draw() {
+        return this.deck.pop();
     }
 }
