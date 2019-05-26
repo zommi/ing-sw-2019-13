7861245446173d0b7d959af9cada1220dcbadf9b
Rambo <teopac165@gmail.com>
2019-05-26 13:52:41 +0200

    edit ShootParser

--- src/main/java/client/InputAbstract.java
+++ src/main/java/client/InputAbstract.java
@@ -4,9 +4,19 @@ import client.weapons.MacroEffect;
 import client.weapons.MicroEffect;
 import client.weapons.Weapon;
 
+import java.util.List;
+
 public interface InputInterface {
     public boolean getChoice(MacroEffect macroEffect);
     public MacroEffect chooseOneMacro(Weapon weapon);
 
     public boolean getChoice(MicroEffect microEffect);
+
+    List<SquareInfo> askSquares(int maxSquares);
+
+    List<String> askPlayers(int maxTargetPlayerSize);
+
+    List<String> askRooms(int maxTargetRoomSize);
+
+
 }
