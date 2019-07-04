274e3589471f174c4f946ffe6826364cd55cc5a5
alessiapacca <alessia.paccagnella@mail.polimi.it>
2019-07-04 16:49:54 +0200

    javadoc for controller

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -8,7 +8,7 @@ import java.util.Properties;
 public class Constants {
 
     //match rules
-    public static final int MIN_PLAYERS = 1;
+    public static final int MIN_PLAYERS = 3;
     public static final int MAX_PLAYERS = 5;
     public static final int MIN_PLAYERS_TO_CONTINUE = 1;
 
