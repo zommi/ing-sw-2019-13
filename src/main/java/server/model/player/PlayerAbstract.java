b83caad86fc3e3c4169d644d08010ac963894888
Tommaso Pegolotti <tommaso.pegolotti@mail.polimi.it>
2019-07-05 11:27:14 +0200

    add javadoc to playerabstract

--- src/main/java/server/model/player/PlayerAbstract.java
+++ src/main/java/server/model/player/PlayerAbstract.java
@@ -61,6 +61,10 @@ public abstract class PlayerAbstract implements Serializable {
 
     public abstract WeaponCard getWeaponCard(Weapon weapon);
 
+    /**
+     * collects ammotile from a square
+     * @param square destination square to collect from
+     */
     public abstract void collect(Square square);
 
     public abstract PowerUpCard getPowerUpCard(PowerUpCard powerUpCard);
@@ -69,6 +73,11 @@ public abstract class PlayerAbstract implements Serializable {
 
     public abstract String getCharacterName();
 
+    /**
+     * Collects a weapon from a spawn point.
+     * @param spawnPoint Destination spawnpoint of ammo.
+     * @param choice Index of weapon to collect
+     */
     public abstract void collect(SpawnPoint spawnPoint, int choice);
 
     public abstract GameCharacter getGameCharacter();
@@ -78,8 +87,6 @@ public abstract class PlayerAbstract implements Serializable {
     public PlayerState getPlayerState() {
         return null;
     }
-
-    public abstract void usePowerup(int powerupIndex);
 
     public abstract PlayerBoard getPlayerBoard();
 
@@ -93,44 +100,99 @@ public abstract class PlayerAbstract implements Serializable {
 
     public abstract void setState(PlayerState state);
 
+    /**
+     * Method that checks if a player can pay a given cost.
+     * @param cost Cost in ammocubes to pay
+     * @return The method returns true if the player can pay the cost.
+     */
     public abstract boolean canPay(Cost cost);
 
     public abstract String getName();
 
+    /**
+     * Add damage to player's playerboard
+     * @param damage number of damage to give to a player.
+     * @param color Color of the attacker.
+     */
     public abstract void addDamage(int damage, Color color);
 
+    /**
+     * Adds marks to a player's playerboard.
+     * @param marks number of marks to add.
+     * @param color color of the marks to add.
+     */
     public abstract void addMarks(int marks, Color color);
 
+    /**
+     * Changes a player's position on the map.
+     * @param square destination square.
+     */
     public abstract void setPosition(SquareAbstract square);
 
     public  abstract PlayerAbstract getJustDamagedBy();
 
     public abstract void setJustDamagedBy(PlayerAbstract playerAbstract);
 
+    /**
+     * Decreases a players ammo.
+     * @param cost ammo to remove.
+     */
     public abstract void pay(Cost cost);
 
+    /**
+     * Method that is used to decrease a cost of a certain effect or weapon
+     * if the client decides to use ammos to pay.
+     * @param cost initial cost to pay.
+     * @param powerUpCards List of powerup chosen by the player.
+     */
     public abstract void payWithPowerUps(Cost cost, List<PowerUpCard> powerUpCards);
 
     public abstract boolean hasPowerUpCards(List<PowerUpCard> powerUpCards);
 
     public abstract boolean hasWeaponCards(List<WeaponCard> weaponCards);
 
+    /**
+     * method that processes a player's death
+     */
     public abstract void die();
 
     public abstract boolean isOverkilled();
 
     public abstract Color getKillerColor();
 
+    /**
+     * Method used to check if a player is connected or not to the server.
+     * It's used to check if a player has to be skipped.
+     * @return returns true if the player is connected.
+     */
     public abstract boolean isActive();
 
     public abstract void setActive(boolean active);
 
+    /**
+     * Adds point to a player during the point distribution phase.
+     * @param i number of points to add.
+     */
     public abstract void addPoints(int i);
 
+    /**
+     * Method used when a player is not active and needs to respawn.
+     * @return A random powerup from a player's hand.
+     */
     public abstract PowerUpCard getRandomPowerupCard();
 
+    /**
+     * Method used to know if a player can play a tagback grenade when shot.
+     * @return number of tagback in hand.
+     */
     public abstract int getNumberOfTagbacks();
 
+    /**
+     * Method used to know if a certain player can play a tagback grenade.
+     * It check if the player has just been shot.
+     * @param currentPlayer player currently playing, he is the only one who can inflict damage.
+     * @return returns true if the player has been damaged by currentplayer
+     */
     public abstract boolean canPlayTagback(PlayerAbstract currentPlayer);
 
     public abstract PlayerState getStateAfterDeath();
